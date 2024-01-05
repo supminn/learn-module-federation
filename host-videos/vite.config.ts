7b04b68606995a4bd690972302509e2cf99fe794
supminn <sansup49@gmail.com>
2024-01-05 11:42:52 +0530

    refactor: build config

--- host-videos/vite.config.ts
+++ host-videos/vite.config.ts
@@ -13,9 +13,7 @@ export default defineConfig({
     shared:["react","react-dom"]
   })],
   build: {
-    modulePreload: false,
     target: "esnext",
-    minify: false,
     cssCodeSplit: false,
   },
 })
