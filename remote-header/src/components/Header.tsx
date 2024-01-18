f414127be5aa13b14cba6293145a3adcb1154d7e
supminn <sansup49@gmail.com>
2024-01-18 20:30:55 +0530

    feat: added state to header

--- remote-header/src/components/Header.tsx
+++ remote-header/src/components/Header.tsx
@@ -1,19 +1,81 @@
-import React from 'react';
-import './Header.css'; // Import the CSS file for styling
-
+import React, { useState } from "react";
+import "./Header.css";
 interface HeaderProps {
-  title: string;
   logo?: string;
+  title: string;
 }
 
 const Header: React.FC<HeaderProps> = ({ logo, title }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
+    isSet: false,
+  });
+
+  const handleLoginSubmit = () => {
+    setCredentials((prev) => ({ ...prev, isSet: true }));
+    setIsModalOpen(false);
+  };
+
   return (
     <header className="navbar">
-      <div className="logo-container">{logo?<img src={logo} className="logo react" alt="React logo" />:<small className='logo-text'>Logo</small>}</div>
+      <div className="logo-container">
+        {logo ? (
+          <img src={logo} className="logo react" alt="React logo" />
+        ) : (
+          <small className="logo-text">Logo</small>
+        )}
+      </div>
       <h1 className="title">{title}</h1>
       <div className="login-btn-container">
-        <button className="login-btn">Login</button>
+        {credentials.isSet ? (
+          <span>Welcome {credentials.username}</span>
+        ) : (
+          <button className="login-btn" onClick={() => setIsModalOpen(true)}>
+            Login
+          </button>
+        )}
       </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="modal-container">
+          <div className="modal">
+            <span className="close" onClick={() => setIsModalOpen(false)}>
+              &times;
+            </span>
+            <h2>Login</h2>
+            <form onSubmit={(e) => e.preventDefault()}>
+              <label>Username:</label>
+              <input
+                type="username"
+                value={credentials.username}
+                onChange={(e) =>
+                  setCredentials((prev) => ({
+                    ...prev,
+                    username: e.target.value,
+                  }))
+                }
+                required
+              />
+              <label>Password:</label>
+              <input
+                type="password"
+                value={credentials.password}
+                onChange={(e) =>
+                  setCredentials((prev) => ({
+                    ...prev,
+                    password: e.target.value,
+                  }))
+                }
+                required
+              />
+              <button onClick={handleLoginSubmit}>Submit</button>
+            </form>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
